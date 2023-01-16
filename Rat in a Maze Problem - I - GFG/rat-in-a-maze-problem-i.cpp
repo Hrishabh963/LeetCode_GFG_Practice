2cab94275b92f1acca3613fbd3c4977f72328ded
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-01-16 17:28:25 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    void solve(int i,int j,vector<vector<int>>&m,int n,vector<string>&ans,string move,vector<vector<int>> &visited){
+        if(i<0 || j<0 || i>=n || j>=n || visited[i][j] || m[i][j] == 0) return;
+        if(i == n-1 && j==n-1){
+            ans.push_back(move);
+            return;
+        }
+        visited[i][j] = 1;
+        solve(i+1,j,m,n,ans,move+'D',visited);
+        solve(i,j-1,m,n,ans,move+'L',visited);
+        solve(i,j+1,m,n,ans,move+'R',visited);
+        solve(i-1,j,m,n,ans,move+'U',visited);
+        visited[i][j] = 0;
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        vector<string> ans;
+        string move;
+        solve(0,0,m,n,ans,move,visited);
+        return ans;
+    }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
