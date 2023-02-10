e89a3f21f1f6bb309d35721860aa624281fcf40b
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-02-10 13:42:32 +0530

    Time: 39 ms (93.03%), Space: 13.4 MB (48.07%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,41 +1,35 @@
-class Solution
-{
-    public:
-        vector<vector < int>> fourSum(vector<int> &nums, int target)
-        {
-            vector<vector < int>> ans;
-            if (nums.empty()) return ans;
-            sort(nums.begin(), nums.end());
-            int n = nums.size();
-            for (int i = 0; i < n; i++)
-            {
-                long long int target2 = target - nums[i];
-                for (int j = i + 1; j < n; j++)
-                {
-                    long long int rem = target2 - nums[j];
-                    int left = j + 1;
-                    int right = n - 1;
-                    while (left < right)
-                    {
-                        long long int two_sum = nums[left] + nums[right];
-                        if (two_sum < rem) left++;
-                        else if (two_sum > rem) right--;
-                        else
-                        {
-                            vector<int> temp(4, 0);
-                            temp[0] = nums[i];
-                            temp[1] = nums[j];
-                            temp[2] = nums[left];
-                            temp[3] = nums[right];
-                            ans.push_back(temp);
-                            while (left < right && nums[left] == temp[2]) left++;
-                            while (left < right && nums[right] == temp[3]) right--;
-                        }
+class Solution {
+public:
+    #define ll long long int
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        ll n = nums.size();
+        for(ll i = 0; i<n; i++){
+            for(ll j = i+1; j<n; j++){
+                ll target2 = nums[i]+nums[j];
+                ll rem = target - (target2);
+                ll low = j+1;
+                ll high = n-1;
+                while(low<high){
+                    ll sum = nums[low]+nums[high];
+                    if(sum<rem)low++;
+                    else if(sum>rem)high--;
+                    else{
+                        vector<int> temp(4,0);
+                        temp[0] = nums[i];
+                        temp[1] = nums[j];
+                        temp[2] = nums[low];
+                        temp[3] = nums[high];
+                        ans.push_back(temp);
+                        while(low<high && nums[low] == temp[2])low++;
+                        while(low<high && nums[high] == temp[3])high--;
                     }
-                    while (j < n - 1 && nums[j] == nums[j + 1]) j++;
-                    while (i < n - 1 && nums[i] == nums[i + 1]) i++;
                 }
+                while(j<n-1 && nums[j]==nums[j+1])j++;
+                while(i<n-1 && nums[i]==nums[i+1])i++;
             }
-            return ans;
         }
+        return ans;
+    }
 };
