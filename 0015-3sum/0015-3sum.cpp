ef1b6f7d4a5fbddfc9b907256d9702faf620bf6d
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-02-10 13:13:02 +0530

    Time: 123 ms (73.93%), Space: 24.1 MB (54.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        for(int i = 0; i<nums.size(); i++){
-            int low = i+1;
-            int high = nums.size()-1;
-            while(low<high){
-            int sum = nums[i]+nums[low]+nums[high];
-            if(sum<0)low++;
-            else if(sum>0)high--;
-            else{
-                ans.push_back({nums[i],nums[low],nums[high]});
-                int low_last = nums[low];
-                int high_last = nums[high];
-                while(low<high && nums[low] == low_last) low++;
-                while(low<high && nums[high] == high_last) high--;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i<n; i++){
+                int low = i+1;
+                int high = n-1;
+                while(low<high){
+                    int sum = nums[i]+nums[low]+nums[high];
+                    if(sum<0)low++;
+                    else if(sum>0)high--;
+                    else{
+                        ans.push_back({nums[i],nums[low],nums[high]});
+                        int currl = nums[low];
+                        int currh = nums[high];
+                        while(low<high && nums[low] == currl)low++;
+                        while(low<high && nums[high] == currh)high--; 
+                    }
+                }
+            while(i<n-1 && nums[i] == nums[i+1])i++;
             }
-            }
-            while(i<nums.size()-1 && nums[i] == nums[i+1]) i++;
-        }
         return ans;
-    }
+        }
 };
