07562dd14defcdb2fbedc7200dd24d8051cc30e5
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-02-25 18:45:17 +0530

    Time: 11 ms (41.12%), Space: 13.8 MB (85.08%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+//     int rec(vector<int> &arr,int i){
+//         if(i<0)return 1;
+//         if(i == 0)return arr[i];
+//         int left =1;
+//         left*=rec(arr,i-1);
+//         int right = 1;
+//         right*= arr[i]*rec(arr,i-1);
+//         return max(left,right);
+          
+//     }
+    int maxProduct(vector<int>& nums) {
+        //vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),1));
+        int maxi = INT_MIN;
+        int curr = 1;
+        for(int i = 0; i<nums.size(); i++){
+            curr*= nums[i];
+            if(curr>maxi){
+                maxi = max(maxi,curr);
+            }
+            if(curr==0){
+                curr = 1;
+            }
+        }
+        curr = 1;
+         for(int i = nums.size()-1; i>=0; i--){
+            curr*= nums[i];
+            if(curr>maxi){
+                maxi = max(maxi,curr);
+            }
+            if(curr==0){
+                curr = 1;
+            }
+        }
+        return maxi;
+    }
+};
