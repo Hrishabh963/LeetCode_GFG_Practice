4a95d05aaeac1338399e7760d263afc84a1c9c78
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-03-14 23:08:01 +0530

    Time: 27 ms (75.24%), Space: 22.5 MB (16.47%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,10 +5,18 @@ public:
         int high = nums.size()-2;
         while(low<=high){
             int mid = (low+high)>>1;
-            if(nums[mid] == nums[mid^1]){
-                low=mid+1;
+            if(mid%2==0){
+                if(nums[mid] == nums[mid+1]){
+                    low = mid+1;
+                }
+                else high = mid-1;
             }
-            else high = mid-1;
+            else{
+                if(nums[mid] == nums[mid+1]){
+                    high = mid-1;
+                }
+                else low = mid+1;
+            }
         }
         return nums[low];
     }
