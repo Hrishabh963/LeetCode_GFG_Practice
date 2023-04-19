fb9398629abf37df44c828d4e8b674b431368d04
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-04-19 13:51:31 +0530

    Time: 197 ms (28.57%), Space: 101.6 MB (41.81%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestZigZag(TreeNode* root) {
+        int maxLen = 0;
+        dfs(root, maxLen);
+        return maxLen;
+    }
+    
+    pair<int, int> dfs(TreeNode* node, int& maxLen) {
+        if (!node) {
+            return {0, 0};
+        }
+        
+        auto left = dfs(node->left, maxLen);
+        auto right = dfs(node->right, maxLen);
+        
+        int leftMax = left.second, rightMax = right.first;
+        maxLen = max(maxLen, max(leftMax, rightMax));
+        
+        return {1 + left.second, 1 + right.first};
+    }
+};
