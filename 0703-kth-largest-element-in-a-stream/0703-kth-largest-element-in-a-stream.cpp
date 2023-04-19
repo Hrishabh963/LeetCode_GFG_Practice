3909ee8442ec82cd54dd290f92512eaf37664243
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-04-20 00:20:47 +0530

    Time: 38 ms (47.24%), Space: 19.8 MB (98.18%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -5,16 +5,16 @@ public:
     KthLargest(int k, vector<int>& nums) {
         this->k = k;
         auto endPointer = nums.begin() + min(k, (int)nums.size());
-        pq = priority_queue<int, vector<int>, greater<int>> (nums.begin(), endPointer);
-        for(int i = k; i < nums.size(); ++i) {
-            pq.push(nums[i]);
-            pq.pop();
+        pq = priority_queue<int, vector<int>, greater<int>> (nums.begin(), endPointer); // Push less than or equal to first k ele in pq 
+         for(int i = k; i < nums.size(); ++i) {
+            pq.push(nums[i]);   //insert the rest
+            pq.pop();           //Pop to maintain exactly less than or equal to k ele in pq
         }
     }
     
     int add(int val) {
         pq.push(val);
-        if(pq.size() > k) pq.pop();
+        if(pq.size() > k) pq.pop();  //pq will hold atmost k+1 ele 
         return pq.top();
     }
 };
