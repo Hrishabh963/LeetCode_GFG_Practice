a4c625ba25d19ce69aa00ee6f3ba28253c635645
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-04-20 23:54:25 +0530

    Time: 383 ms (56.00%), Space: 144.6 MB (53.45%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
     
-//     int rec(vector<int> &arr,int i,int n,int buy,int cap,vector<vector<vector<int>>> &dp){
-//         if(cap==0)return 0;
-//         if(i==n)return 0;
-//         if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
-//         int profit = 0;
-//         if(buy){
-//             profit = max(rec(arr,i+1,n,0,cap,dp)-arr[i],rec(arr,i+1,n,1,cap,dp));
-//         }
-//         else{
-//             profit = max(rec(arr,i+1,n,1,cap-1,dp)+arr[i],rec(arr,i+1,n,0,cap,dp));
-//         }
-//         return dp[i][buy][cap] = profit;
-//     }
-    
-    int maxProfit(vector<int>& v) {
-        int n = v.size();
-        int notpick,pick;
-        vector<vector<vector<int>>> dp (n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                for(int k=1;k<=2;k++){
-                    if(j){
-                        notpick = 0 + dp[i+1][1][k];
-                        pick = v[i] + dp[i+1][0][k-1];
-                    }
-                    else{
-                        notpick = 0 + dp[i+1][0][k];
-                        pick = -v[i] + dp[i+1][1][k];
-                    }
-                    dp[i][j][k] = max(pick,notpick);
-                }
-            }
+    int f(vector<int> &prices,int i,int currTrans,int cap,vector<vector<int>> &dp){
+        if(i==prices.size() || currTrans == 2*cap) return 0;
+        if(dp[i][currTrans]!=-1)return dp[i][currTrans];
+        if(currTrans%2==0){
+            return dp[i][currTrans] = max(-prices[i]+f(prices,i+1,currTrans+1,cap,dp),f(prices,i+1,currTrans,cap,dp));
         }
-        return dp[0][0][2];
+        return dp[i][currTrans] = max(prices[i]+f(prices,i+1,currTrans+1,cap,dp),f(prices,i+1,currTrans,cap,dp));
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size()+1,vector<int>(5,-1));
+        return f(prices,0,0,2,dp);
     }
 };
