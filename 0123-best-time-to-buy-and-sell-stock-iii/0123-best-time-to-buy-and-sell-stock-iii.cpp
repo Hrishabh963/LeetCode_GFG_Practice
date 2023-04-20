47cc20be085fa3eb3c90896593d6d721ccb70596
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-04-20 23:56:57 +0530

    Time: 321 ms (59.63%), Space: 135.2 MB (56.15%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     
-    int f(vector<int> &prices,int i,int currTrans,int cap,vector<vector<int>> &dp){
-        if(i==prices.size() || currTrans == 2*cap) return 0;
-        if(dp[i][currTrans]!=-1)return dp[i][currTrans];
-        if(currTrans%2==0){
-            return dp[i][currTrans] = max(-prices[i]+f(prices,i+1,currTrans+1,cap,dp),f(prices,i+1,currTrans,cap,dp));
-        }
-        return dp[i][currTrans] = max(prices[i]+f(prices,i+1,currTrans+1,cap,dp),f(prices,i+1,currTrans,cap,dp));
-    }
+    // int f(vector<int> &prices,int i,int currTrans,int cap,vector<vector<int>> &dp){
+    //     if(i==prices.size() || currTrans == 2*cap) return 0;
+    //     if(dp[i][currTrans]!=-1)return dp[i][currTrans];
+    //     if(currTrans%2==0){
+    //         return dp[i][currTrans] = max(-prices[i]+f(prices,i+1,currTrans+1,cap,dp),f(prices,i+1,currTrans,cap,dp));
+    //     }
+    //     return dp[i][currTrans] = max(prices[i]+f(prices,i+1,currTrans+1,cap,dp),f(prices,i+1,currTrans,cap,dp));
+    // }
     
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size()+1,vector<int>(5,-1));
-        return f(prices,0,0,2,dp);
+        vector<vector<int>> dp(prices.size()+1,vector<int>(5,0));
+        for(int i =prices.size()-1; i>=0; i--){
+            for(int j = 3;j>=0; j--){
+                if(j%2==0){
+                    dp[i][j] = max(-prices[i]+dp[i+1][j+1],dp[i+1][j]);
+                }
+                else dp[i][j] = max(prices[i]+dp[i+1][j+1],dp[i+1][j]);
+            }
+        }
+        return dp[0][0];
     }
 };
