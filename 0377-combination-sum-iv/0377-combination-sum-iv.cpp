c576c4ef69b362ad0a6da164d1b1a3c4a4559f6d
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-05-14 17:58:48 +0530

    Time: 5 ms (32.79%), Space: 6.6 MB (93.18%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int f(vector<int> &nums,int i,int target,vector<vector<int>> &dp){
+    //2D dp approach
+    // int f(vector<int> &nums,int i,int target,vector<vector<int>> &dp){
+    //     if(target==0)return 1;
+    //     if(i>=nums.size()) return 0;
+    //     if(dp[i][target]!=-1) return dp[i][target];
+    //     int notTake = 0, take = 0;
+    //     notTake = f(nums,i+1,target,dp);
+    //     if(nums[i]<=target) take = f(nums,0,target - nums[i],dp);
+    //     return dp[i][target] = take+notTake;
+    // }
+    int f(vector<int> &arr,int target ,vector<int> &dp){
         if(target==0)return 1;
-        if(i>=nums.size()) return 0;
-        if(dp[i][target]!=-1) return dp[i][target];
-        int notTake = 0, take = 0;
-        notTake = f(nums,i+1,target,dp);
-        if(nums[i]<=target) take = f(nums,0,target - nums[i],dp);
-        return dp[i][target] = take+notTake;
+        if(dp[target]!=-1) return dp[target];
+        int ans = 0;
+        for(int i = 0; i<arr.size(); i++){
+            if(arr[i]<=target) ans+=f(arr,target-arr[i],dp);
+        }
+        return dp[target] = ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
-        return f(nums,0,target,dp);
+        vector<int> dp(target+1,-1);
+        return f(nums,target,dp);
     }
 };
