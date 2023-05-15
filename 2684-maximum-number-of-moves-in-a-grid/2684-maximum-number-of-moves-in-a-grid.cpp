de98d93a267f5573f88c5de0743a701e5fdfdcfa
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-05-15 14:07:16 +0530

    Time: 238 ms (69.23%), Space: 69.1 MB (23.08%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,22 +1,27 @@
 class Solution {
-    int dfs(int n, int m, vector<vector<int>>& grid, int i, int j, int prev) {
-        if(i < 0 or j < 0 or i >= n or j >= m or grid[i][j] <= prev)return 0;
-        
-        int val = grid[i][j];
-        grid[i][j] = 0;
-        
-        int ans = max({dfs(n,m,grid,i-1,j+1,val), dfs(n,m,grid,i,j+1,val),dfs(n,m,grid,i+1,j+1,val)}) + 1;
-        
-        return ans;
-    }
 public:
+    int dfs(int i, int j, int n, int m, vector<vector<int>> &grid, vector<vector<int>> &dp) {
+       
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int first = 0, second = 0, third = 0;
+        if(i>=1 and j<m-1 and grid[i][j] < grid[i - 1][j + 1])
+            first = 1 + dfs(i - 1, j + 1, n, m, grid, dp);
+        if(j<m-1 and grid[i][j] < grid[i][j + 1])
+            second = 1 + dfs(i, j + 1, n, m, grid, dp);
+        if(i<n-1 and j<m-1 and grid[i][j] < grid[i + 1][j + 1]) 
+            third = 1 + dfs(i + 1, j + 1, n, m, grid, dp);
+        return dp[i][j] = max(first,max(second,third));
+    }
+
     int maxMoves(vector<vector<int>>& grid) {
-        int n = size(grid), m = size(grid[0]), res = 0;
+        int n = grid.size(), m = grid[0].size(), ans = 0;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
         
-        for(int i = 0; i < n; i++) {
-            res = max(res,dfs(n,m,grid,i,0,-1e6));
+        for(int j=0; j<n; j++) {
+            ans = max(ans, dfs(j, 0, n, m, grid, dp));
         }
+        return ans;
         
-        return res - 1;
     }
 };
