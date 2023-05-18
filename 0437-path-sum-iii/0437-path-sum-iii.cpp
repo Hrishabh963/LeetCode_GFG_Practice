b2aa193155539ad17c040773e0291e792fa3e6bb
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2023-05-18 23:58:39 +0530

    Time: 12 ms (96.54%), Space: 19.1 MB (45.59%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,24 +11,39 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root,long long int sum,int target,int &ans){
-        if(!root)return;
+    // O(n^2) solution 
+    // void helper(TreeNode* root,long long int sum,int target,int &ans){
+    //     if(!root)return;
+    //     sum+=root->val;
+    //     if(sum == target)
+    //         ans++;
+    //     helper(root->left,sum,target,ans);
+    //     helper(root->right,sum,target,ans);
+    // }
+    // void dfs(TreeNode* root,int target,int &ans){
+    //     if(!root)return;
+    //     long long int sum  = 0;
+    //     helper(root,sum,target,ans);
+    //     dfs(root->left,target,ans);
+    //     dfs(root->right,target,ans);
+    // }
+    int ans = 0;
+    void dfs(TreeNode* root,long long int sum,int target,unordered_map<long long int,int> &m){
+        if(!root) return;
         sum+=root->val;
-        if(sum == target)
-            ans++;
-        helper(root->left,sum,target,ans);
-        helper(root->right,sum,target,ans);
-    }
-    void dfs(TreeNode* root,int target,int &ans){
-        if(!root)return;
-        long long int sum  = 0;
-        helper(root,sum,target,ans);
-        dfs(root->left,target,ans);
-        dfs(root->right,target,ans);
+        if(sum == target) ans++;
+        if(m.find(sum - target)!=m.end()){
+            ans+= m[sum-target];
+        }
+        m[sum]++;
+        dfs(root->left,sum,target,m);
+        dfs(root->right,sum,target,m);
+        m[sum]--;
     }
     int pathSum(TreeNode* root, int targetSum) {
-        int ans = 0;
-        dfs(root,targetSum,ans);
+        //we can use map to store the current path sum,use count subarray sum logic to count the path sums
+        unordered_map<long long int,int> m;
+        dfs(root,0,targetSum,m);
         return ans;
         
     }
